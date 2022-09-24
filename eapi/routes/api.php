0e5c54935927c43df6790d4600c657eef41f821f
aykacx <halilaykac@doamedia.co>
2022-09-24 23:48:17 +0300

    product controller

--- eapi/routes/api.php
+++ eapi/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-
+use App\Http\Controllers\ReviewController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -11,4 +11,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('/products',ProductController::class);
 
-
+Route::group(['prefix'=>'products'], function(){
+    Route::apiResource('/{product}/reviews',ReviewController::class);
+});
